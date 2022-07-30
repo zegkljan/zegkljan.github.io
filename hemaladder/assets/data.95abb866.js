4154e30894ba853bdbf27ed1078c2ef7422ccf05
zegkljan <zegkljan@gmail.com>
2022-07-30 02:22:53 +0200

    General overhaul, season 2022/2023.
    * Rewritten so that the ladder is precomputed.
    * Removed non-czech fencers and clubs.
    * Added season 2022/2023.

---
+++ hemaladder/assets/data.95abb866.js
@@ -0,0 +1,1 @@
+import{b as o,u as t,l as e,j as l,k as d}from"./index.f55f6cd1.js";var u=o(async({store:s})=>{const a=t(s);a.clubs=await e(),a.people=await l(),a.seasons=await d()});export{u as default};
--- hemaladder/assets/data.a3f890ef.js
+++
@@ -1,1 +0,0 @@
-import{j as t}from"./index.f5c5c260.js";const a=t("data",{state:()=>({people:{},clubs:{},tournaments:{}}),getters:{},actions:{}});export{a as u};

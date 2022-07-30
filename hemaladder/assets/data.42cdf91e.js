4dec2c1a66de8fd77f4ca3af545eb2cecfc4e23c
zegkljan <zegkljan@gmail.com>
2022-07-30 03:20:58 +0200

    Rebuild without public path.

---
+++ hemaladder/assets/data.42cdf91e.js
@@ -0,0 +1,1 @@
+import{b as o,u as t,l as e,j as l,k as d}from"./index.14aec6da.js";var u=o(async({store:s})=>{const a=t(s);a.clubs=await e(),a.people=await l(),a.seasons=await d()});export{u as default};
